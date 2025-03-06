761b289934c98767ac01a887ed5d210eb724143d
w1zzball <aaronibbo@gmail.com>
2025-03-06 10:30:22 +0000

    Add posts app with Post model and initial migration

---
+++ posts/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Post(models.Model):
+    """Model for blog posts"""
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        'profiles.Profile',
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+
+    def __str__(self):
+        return f"{self.body[:50]}... | {self.author}"
