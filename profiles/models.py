4a34588b4dbb9334f7e1ca41f2c4fe31473caaa1
w1zzball <aaronibbo@gmail.com>
2025-03-14 13:00:54 +0000

    feat: implement delete functionality for posts own pages with confirmation and AJAX request

--- profiles/models.py
+++ profiles/models.py
@@ -12,9 +12,7 @@ from cloudinary.models import CloudinaryField
 class Profile(models.Model):
     """ extends the user model with additional fields"""
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # email = models.EmailField(max_length=254, blank=True)
     bio = models.TextField(max_length=500, blank=True)
-    # TODO implement profile picture with cloudinary
     profile_pic = CloudinaryField('image', default='placeholder')
     # default user model has
     # date_joined field
