272ee41f8065c5260171286bc31c4dd53eb7de1d
w1zzball <aaronibbo@gmail.com>
2025-03-24 15:48:32 +0000

    fix: added extra security to prevent accidental debug mode

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEBUG' in os.environ
+DEBUG = 'DEBUG' in os.environ and os.environ['DEBUG'] == 'True'
 print(f"DEBUG: {DEBUG}")
 ALLOWED_HOSTS = ['.herokuapp.com',
                  'localhost',
