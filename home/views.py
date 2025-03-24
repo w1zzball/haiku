be15d89a5008b708d80e8340ef3d3346a90a7544
w1zzball <aaronibbo@gmail.com>
2025-03-24 20:57:39 +0000

    fix: show selection of haikus

--- home/views.py
+++ home/views.py
@@ -11,19 +11,13 @@ def home_view(request):
     context = {}
 
     if request.user.is_authenticated:
-        # For logged-in users, display personalized content
-        user_profile = request.user.profile
-        user_posts = Post.objects.filter(
-            author=user_profile).order_by('-created_at')[:5]
-        context['user_posts'] = user_posts
         context['page_title'] = "Your Feed"
     else:
-        # For anonymous users, display random selection of posts
-        all_posts = list(Post.objects.all())
-        if all_posts:
-            # Get up to 5 random posts, or all if less than 5 exist
-            random_posts = random.sample(all_posts, min(5, len(all_posts)))
-            context['random_posts'] = random_posts
         context['page_title'] = "Welcome to Haiku"
+    all_posts = list(Post.objects.all())
+    if all_posts:
+        # Get up to 5 random posts, or all if less than 5 exist
+        random_posts = random.sample(all_posts, min(5, len(all_posts)))
+        context['random_posts'] = random_posts
 
     return render(request, 'home/index.html', context)
